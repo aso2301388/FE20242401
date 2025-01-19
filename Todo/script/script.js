4f44cc15c30c53b6a445b00c21b60f0008c2f298
aso2301388 <2301388@s.asojuku.ac.jp>
2025-01-20 02:28:45 +0900

    a
    aaaaa

--- Todo/script/script.js
+++ Todo/script/script.js
@@ -2,16 +2,40 @@ new Vue({
   el: '#app',
   data() {
     return {
-      //データの準備
-
+      todos: [],
+      text: ''
     };
   },
+  computed: {
+    doneTodo() {
+      return this.todos.filter(todo => todo.isDone === true);
+    },
+    incompleteTodo() {
+      return this.todos.filter(todo => todo.isDone === false);
+    }
+  },
   methods: {
-    //inputTextメソッドを追加
-
-    //addTodoメソッドを追加
-
-    //resetTextメソッドを追加
-    
+    inputText(e) {
+      this.text = e.target.value;
+    },
+    addTodo() {
+      if (!this.text) return;
+      const id = Math.ceil(Math.random() * 1000);
+      this.todos.push({
+        id,
+        text: this.text,
+        isDone: false
+      });
+      this.text = '';
+    },
+    toggleIsDone(id) {
+      const todo = this.todos.find(todo => todo.id === id);
+      if (todo) {
+        todo.isDone = !todo.isDone;
+      }
+    },
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
 });
